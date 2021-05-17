bf12b766555c734f4434f727d18393eba7a3530e
daniil <4shushakov@gmail.com>
2021-05-17 11:37:21 +0700

    convert double values to int, add struct Cell

--- main.cpp
+++ main.cpp
@@ -12,9 +12,16 @@ struct Indexes {
     int i, j;
 };
 
+struct Cell {
+    int value = 0, cost = 0;
+    bool isBasic = false;
+};
+
 class TransportTask {
+    typedef vector<vector<Cell>> cellMatrix;
+
 public:
-    dmatrix FindBasePlan() {
+    cellMatrix FindBasePlan() {
         if (IsOpenModel())
             ConvertOpenModelToClosed();
         if (!sorted) {
@@ -25,20 +32,22 @@ public:
         for (auto ind: fieldsSortedByC) {
             int i = ind.i, j = ind.j;
 
-            if (abs(ac[i] - bc[j]) < eps / m && ac[i] > eps)
-                // Set visible cell value in row to eps
+            if (ac[i] == bc[j] && ac[i] > 0)
+                // Set visible cell value in row to basic
                 for (int col = 0; col < n; col++)
-                    if (b[col] > 0) {
-                        x[i][col] += eps * i / m;
+                    if (bc[col] > 0) {
+                        cells[i][col].isBasic = true;
                         break;
                     }
 
-            db delta = min(ac[i], bc[j]);
-            x[i][j] += delta;
+            int delta = min(ac[i], bc[j]);
+            cells[i][j].value += delta;
+            if (delta > 0)
+                cells[i][j].isBasic = true;
             ac[i] -= delta;
             bc[j] -= delta;
         }
-        return x;
+        return cells;
     }
 
     void Input() {
@@ -50,13 +59,10 @@ public:
         b.resize(n);
         for (int i = 0; i < n; i++)
             fin >> b[i];
-        c.resize(m);
-        for (int i = 0; i < m; i++) {
-            c[i].resize(n);
+        cells = cellMatrix(m, vector<Cell>(n));
+        for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
-                fin >> c[i][j];
-        }
-        x = dmatrix(m, dvector(n, 0));
+                fin >> cells[i][j].cost;
         fin.close();
     }
 
@@ -68,10 +74,13 @@ public:
             cout << setw(wid) << "B_" + to_string(i + 1);
         cout << setw(wid) << "Запасы" << endl;
 
-        for (int i = 0; i < x.size(); i++) {
+        for (int i = 0; i < m; i++) {
             cout << setw(firstWid) << "A_" + to_string(i + 1);
-            for (int j = 0; j < x[0].size(); j++)
-                printf("%8g", round(x[i][j] * pow(10, prc)) / pow(10, prc));
+            for (int j = 0; j < n; j++)
+                if (cells[i][j].isBasic && cells[i][j].value == 0)
+                    printf("%*s", wid, "eps");
+                else
+                    printf("%*g", wid, round(cells[i][j].value * pow(10, prc)) / pow(10, prc));
             cout << setw(wid) << a[i] << endl;
         }
         cout << setw(firstWid) << "Потребности";
@@ -81,7 +90,7 @@ public:
         db cost = 0;
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
-                cost += c[i][j] * x[i][j];
+                cost += cells[i][j].cost * cells[i][j].value;
         cout << "\nОбщая стоимость: " << cost;
     }
 
@@ -92,7 +101,9 @@ private:
             for (int j = 0; j < n; j++)
                 fieldsSortedByC[i * n + j] = {i, j};
         sort(fieldsSortedByC.begin(), fieldsSortedByC.end(),
-             [this](Indexes first, Indexes second) { return c[first.i][first.j] < c[second.i][second.j]; });
+             [this](Indexes first, Indexes second) {
+                 return cells[first.i][first.j].cost < cells[second.i][second.j].cost;
+             });
     }
 
     bool IsOpenModel() {
@@ -104,26 +115,23 @@ private:
     }
 
     void ConvertOpenModelToClosed() {
-        db sumA = accumulate(a.begin(), a.end(), 0.);
-        db sumB = accumulate(b.begin(), b.end(), 0.);
-        if (sumB - sumA > eps) {
+        int sumA = (int) accumulate(a.begin(), a.end(), 0.);
+        int sumB = (int) accumulate(b.begin(), b.end(), 0.);
+        if (sumB > sumA) {
             a.push_back(sumB - sumA);
-            x.push_back(dvector(n, 0));
-            c.push_back(dvector(n, 0));
+            cells.push_back(vector<Cell>(n));
             m++;
-        } else if (sumA - sumB > eps) {
+        } else if (sumA > sumB) {
             b.push_back(sumA - sumB);
-            for (int i = 0; i < n; i++) {
-                x[i].push_back(0);
-                c[i].push_back(0);
-            }
+            for (int i = 0; i < n; i++)
+                cells[i].push_back(Cell{});
             n++;
         }
     }
 
     int m = 0, n = 0;
-    dvector a, b;
-    dmatrix c, x;
+    vector<int> a, b;
+    cellMatrix cells;
     vector<Indexes> fieldsSortedByC;
     bool sorted = false;
     db eps = 1e-2;
